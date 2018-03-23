7bb4e3e755c7cdafdc993b4549c2eff6715217bd
huangzhhui <huangzhwork@gmail.com>
2018-03-24 01:58:19 +0800

    init

---
+++ src/Tracer.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Swoft\Trace;
+
+/**
+ * @uses      Tracer
+ * @version   2018年03月24日
+ * @author    huangzhhui <huangzhwork@gmail.com>
+ * @copyright Copyright 2010-2018 Swoft software
+ * @license   PHP Version 7.x {@link http://www.php.net/license/3_0.txt}
+ */
+class Tracer
+{
+
+}
